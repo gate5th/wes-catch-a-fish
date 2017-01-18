6f172493c009f43bd1545b859367bc4c31f09d0b
Michael Bruns <Michael Bruns>
2017-01-17 20:42:42 -0600

    created StorePicker component (just starting) but finally understand how to link files now!

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,11 @@
 // let's go!
+import React from 'react';
+import ReactDOM from 'react-dom';
+import StorePicker from'./components/StorePicker';
+
+
+
+ReactDOM.render(
+  <StorePicker/>,
+  document.getElementById('main')
+);
