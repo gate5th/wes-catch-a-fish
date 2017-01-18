97965fe9bb54de5401b04fb815b396d783cb9314
Michael Bruns <Michael Bruns>
2017-01-18 12:21:05 -0600

    tied addfish component up to App with state. Pretty confusing, and expect will be different procedure with redux, but go back to last commit and redo lesson 13 if need better understanding

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -13,19 +13,21 @@ class AddFishForm extends React.Component{
       image: this.image.value,
     }
     console.log(fish);
+    this.props.addFish(fish); //uses the addFish function passed down from App (via Inventory and AddFishForm) to modify state at the App level
+    this.fishForm.reset() //clears out the form after all other actions taken
   }
 
   render() {
     return (
-      <form className="fish-edit" onSubmit={this.createFish.bind(this)}>
-        <input ref={(input) => {this.name = input}}type="text" placeholder="Fish Name" />
-        <input ref={(input) => {this.price = input}}type="text" placeholder="Fish Price" />
+      <form ref={(input) => {this.fishForm = input}} className="fish-edit" onSubmit={this.createFish.bind(this)}>
+        <input ref={(input) => {this.name = input}} type="text" placeholder="Fish Name" />
+        <input ref={(input) => {this.price = input}} type="text" placeholder="Fish Price" />
         <select ref={(input) => {this.status = input}}>
         <option value="available">Fresh!</option>
         <option value="unavailable">Sold Out!</option>
         </select>
-        <textarea ref={(input) => {this.desc = input}}placeholder="Fish Desc" />
-        <input ref={(input) => {this.image = input}}type="text" placeholder="Fish Image" />
+        <textarea ref={(input) => {this.desc = input}} placeholder="Fish Desc" />
+        <input ref={(input) => {this.image = input}} type="text" placeholder="Fish Image" />
         <button type="submit">+ Add Item</button>
       </form>
     )
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,36 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component{
+
+  constructor() {
+    //have to inherit methods and such from the React.Component object
+    super();
+
+    //have to bind each class method (except render) to the component
+    this.addFish = this.addFish.bind(this);
+
+    //same as getInitialState
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+  }
+
+  addFish(fish) {  //modifies state when add a fish
+
+    const fishes = {...this.state.fishes}; //copy state first in case screw up
+    const timestamp = Date.now(); //create a timestampe to use as id
+    fishes[`fish-${timestamp}`] = fish;  //adds new element to fishes (which is a copy of pre-transaction state)
+
+    //this command actually updates the state. This refers to
+    // the App component, setState is a react function that updates
+    //the state. The argument is an object with a key being what
+    //part of the state to update (the fishes subobject) and a value
+    //showing what to replace that subobject with (our new fishes object)
+
+    this.setState({fishes: fishes })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +41,7 @@ class App extends React.Component{
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
